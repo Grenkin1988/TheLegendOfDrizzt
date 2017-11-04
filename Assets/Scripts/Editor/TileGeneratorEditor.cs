65e28d00b95d34e202c4ace1bb6b1fd24abbb53a
Gleb Kruglov <kruglovgleb@gmail.com>
2017-11-04 22:48:17 +0200

    Add editor

---
+++ Assets/Scripts/Editor/TileGeneratorEditor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Model;
+
+public class TileGeneratorEditor : EditorWindow {
+    private string layout;
+
+    [MenuItem("Window/Tile Generator")]
+    private static void Init() {
+        GetWindow(typeof(TileGeneratorEditor)).Show();
+    }
+
+    private void OnGUI() {
+        layout = EditorGUILayout.TextField("Layout", layout);
+
+        if (GUILayout.Button("Clear")) {
+            Clear();
+        }
+
+        if (GUILayout.Button("Wall")) {
+            AddSquare(TerrainTypes.Wall);
+        }
+
+        if (GUILayout.Button("Floor")) {
+            AddSquare(TerrainTypes.Floor);
+        }
+
+        if (GUILayout.Button("Mashrooms")) {
+            AddSquare(TerrainTypes.Mashrooms);
+        }
+
+        if (GUILayout.Button("VolcanicVent")) {
+            AddSquare(TerrainTypes.VolcanicVent);
+        }
+
+        if (GUILayout.Button("Copy")) {
+            CopyToClipboard();
+        }
+    }
+
+    private void Clear() {
+        layout = string.Empty;
+    }
+
+    private void AddSquare(TerrainTypes type) {
+        if (string.IsNullOrEmpty(layout)) {
+            layout += type;
+        } else {
+            layout += $";{type}";
+        }
+    }
+
+    private void CopyToClipboard() {
+        var te = new TextEditor();
+        te.text = layout;
+        te.SelectAll();
+        te.Copy();
+    }
+}
