b78cafa3660ca6d9cdd8bd4f1967c4347cadcc2b
Gleb Kruglov <kruglovgleb@gmail.com>
2017-11-05 16:37:07 +0200

    New Tiles

--- Assets/Scripts/Model/SpriteManager.cs
+++ Assets/Scripts/Model/SpriteManager.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using Random = System.Random;
 
 namespace TheLegendOfDrizzt.Assets.Scripts.Model {
     public class SpriteManager {
@@ -12,6 +13,7 @@ namespace TheLegendOfDrizzt.Assets.Scripts.Model {
         private static readonly object _padlock = new object();
         private const string TILE_SPRITE_PATH = "Images/Tiles";
         private const string DEFAULT_SPRITE_NAME = "Default";
+        private const char NAME_SEPARATOR = '_';
 
         public static SpriteManager Instance {
             get {
@@ -26,9 +28,10 @@ namespace TheLegendOfDrizzt.Assets.Scripts.Model {
             }
         }
 
-        private readonly Dictionary<string, Sprite> _spritesMap = new Dictionary<string, Sprite>();
+        private readonly Random _random = new Random();
+        private readonly Dictionary<string, List<Sprite>> _spritesMap = new Dictionary<string, List<Sprite>>();
 
-        public IReadOnlyDictionary<string, Sprite> SpritesMap => new ReadOnlyDictionary<string, Sprite>(_spritesMap);
+        public IReadOnlyDictionary<string, List<Sprite>> SpritesMap => new ReadOnlyDictionary<string, List<Sprite>>(_spritesMap);
 
         private SpriteManager() {
             LoadSprites();
@@ -36,16 +39,32 @@ namespace TheLegendOfDrizzt.Assets.Scripts.Model {
 
         public Sprite LoadSpriteByName(string name) {
             if (_spritesMap.ContainsKey(name)) {
-                return _spritesMap[name];
+                List<Sprite> list = _spritesMap[name];
+                int index = 0;
+                if (list.Count > 1) {
+                    index = _random.Next(list.Count);
+                }
+                return _spritesMap[name][index];
             }
-            return _spritesMap[DEFAULT_SPRITE_NAME];
+            return _spritesMap[DEFAULT_SPRITE_NAME].First();
         }
 
         private void LoadSprites() {
             Sprite[] sprites = Resources.LoadAll<Sprite>(TILE_SPRITE_PATH);
             foreach (Sprite sprite in sprites) {
-                _spritesMap[sprite.name] = sprite;
+                string name = GetSpriteName(sprite);
+                if (!_spritesMap.ContainsKey(name)) {
+                    _spritesMap[name] = new List<Sprite>();
+                }
+                _spritesMap[name].Add(sprite);
             }
+        }
+
+        private string GetSpriteName(Sprite sprite) {
+            if (sprite.name.Contains(NAME_SEPARATOR)) {
+                return sprite.name.Split(NAME_SEPARATOR)[0];
+            }
+            return sprite.name;
         }
     }
 }
