a1890284b29622378025d045da572f627853a1c1
Gleb Kruglov <kruglovgleb@gmail.com>
2017-11-07 20:48:05 +0200

    MainMenu added

---
+++ Assets/Scripts/Controller/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheLegendOfDrizzt.Assets.Scripts.Controller {
+    public class LevelManager : MonoBehaviour {
+        private const string ADVENTURE_SCENE = "Adventure";
+
+        public void LoadAdventure(string name) {
+            Debug.Log($"Load adventure {name}");
+            AdventureController.CurrentAdventureName = name;
+            SceneManager.LoadScene(ADVENTURE_SCENE);
+        }
+
+        public void QuitRequest() {
+            Debug.Log("Quit requsted");
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
